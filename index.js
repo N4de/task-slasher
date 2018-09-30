59fea41f73083d5643c39dda344abfcd4ea3ff21
N4de <jaAnnala@gmail.com>
2018-09-30 10:28:55 +0300

    preparing authentication

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+
+// 587078935800-kttmenlro3auc1h6a1rj3e91f16k41d7.apps.googleusercontent.com client ID
+
+// GyBfUISBfqqosXosxt3-CEE4 client secret!!!
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });

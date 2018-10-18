3a90f00c54b4dc8e8cc9d7981bbb0ba84796da72
N4de <jaAnnala@gmail.com>
2018-10-18 21:12:13 +0300

    authentication callback stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20')
+    .Strategy;
 const keys = require('./config/keys');
 
 const app = express();
@@ -11,13 +12,23 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
 }));
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-})
+    res.send({
+        hi: 'there'
+    });
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

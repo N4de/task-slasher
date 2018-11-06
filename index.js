969015628d11a0fbc1dfb6d6744fef21f286dfe3
N4de <jaAnnala@gmail.com>
2018-11-06 20:19:57 +0200

    change file structure, init sequelize

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20')
-    .Strategy;
-const keys = require('./config/keys');
+const databaseKeys = require('./config/database');
+require('./services/passport');
 
 const app = express();
 
-
-
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-}));
-
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'there'
-    });
-});
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(databaseKeys.databaseName, databaseKeys.databaseUsername, databaseKeys.databasePassword, {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });

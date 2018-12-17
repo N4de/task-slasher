21ec37f56c05d5315e05a13ffa0c8c0a0d7c903f
N4de <jaAnnala@gmail.com>
2018-12-17 19:29:57 +0200

    make test model to database

--- sequelize.js
+++ sequelize.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const User = require('./server/models/user');
+// const User = require('./server/models/user');
 const databaseKeys = require('./config/database');
 
 const sequelize = new Sequelize(databaseKeys.databaseName, databaseKeys.databaseUsername, databaseKeys.databasePassword, {
@@ -16,9 +16,20 @@ const sequelize = new Sequelize(databaseKeys.databaseName, databaseKeys.database
     },
 });
 
+const User = sequelize.define("testdude", {
+    name: Sequelize.STRING,
+    description: Sequelize.TEXT
+});
+
 sequelize.sync()
     .then(() => {
-        console.log(
-            'seqiesöozez'
-        );
+        console.log('things have been synced yes');
+        return User.create({
+            name: 'John',
+            description: 'cool dude'
+        });
     });
+
+    User.findAll().then(users => {
+        console.log(users);
+      });

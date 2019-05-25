5691c734952a6eb4453db18c6c3bab7e35a2ea29
N4de <jaAnnala@gmail.com>
2019-05-25 12:28:45 +0300

    remove old stuff

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 require('./sequelize');
 
 const app = express();
 
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-);
 
-
-require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

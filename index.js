f7e64f5e6c410df1771d91e5fda30bee71cdec05
N4de <jaAnnala@gmail.com>
2019-06-10 20:11:53 +0300

    add auth0 stuff

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const keys = require('./config/keys');
 require('./sequelize');
+const ManagementClient = require('auth0').ManagementClient;
+
 
 const app = express();
 
+const auth0 = new ManagementClient({
+    domain: 'dev-gk1npnde.eu.auth0.com',
+    clientId: 'a6uoCdWsnfgC0NSwc90dhSt8rTFl4SSl',
+    clientSecret: keys.auth0SecretKey,
+  });
 
 
 const PORT = process.env.PORT || 5000;
